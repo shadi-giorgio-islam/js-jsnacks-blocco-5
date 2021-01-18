144195b4ec05f3d1b29df9f3fd2742aa0c4b9fa0
Giorgio <shadigiorgioislam@gmail.com>
2021-01-18 14:44:58 +0100

    jsnack 1 END

--- Jsnack1/js/script.js
+++ Jsnack1/js/script.js
@@ -1,37 +1,13 @@
-// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
-// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-// Generare numeri random al posto degli 0 nelle proprietà:
-// Punti fatti e falli subiti
+// Jsnack1 - Creare un oggetto palla che abbia le seguenti proprietà.
+// Nome = palla
+// Peso = 10
+// Attraverso un prompt dare la possibilità all'utente di modificare il peso della palla.
 
-var squadre = [
-  {
-    nome: 'Inter',
-    punti: 0,
-    falli: 0
-  },
-  {
-    nome: 'Juventus',
-    punti: 0,
-    falli: 0
-  },
-  {
-    nome: 'Milan',
-    punti: 0,
-    falli: 0
-  }
-];
+var palla = {
+  nome : palla,
+  peso : 10
+};
 
-for (var i = 0; i < squadre.length; i++) {
-  squadre[i].punti = generaRandom(0, 99);
-  squadre[i].falli = generaRandom(0, 99);
-  for(var k in squadre[i]){
-    console.log(k,': ',squadre[i][k]);
-  }
-}
+palla.peso = parseInt(prompt('Inserisci peso'));
 
-// console.log(squadre);
-
-// ****FUNZIONI****
-function generaRandom(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
+console.log(palla);
